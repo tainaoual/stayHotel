85b5a2f1a6690fbea985181ac562132360e5631c
tainaoual <tainaoual@gmail.com>
2021-09-24 10:58:35 -0400

    first commit

---
+++ src/main/java/com/example/stayhotel/StayHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.stayhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stayhotel/StayHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stayhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
